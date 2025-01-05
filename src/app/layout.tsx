b2a107098d88c9e34740a487c2ab10ae87eb4f69
sohag-chowwdhury <sohag02.c@gmail.com>
2025-01-06 00:32:49 +0600

    upadte GA4 Code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import { type Metadata } from 'next'
 
 import { RootLayout } from '@/components/RootLayout'
+import GTMScript from '../components/GTM';
 
 import '@/styles/tailwind.css'
 
@@ -14,7 +15,11 @@ export const metadata: Metadata = {
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" className="h-full bg-neutral-950 text-base antialiased">
+        <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </head>
       <body className="flex min-h-full flex-col">
+        <GTMScript />
         <RootLayout>{children}</RootLayout>
       </body>
     </html>
