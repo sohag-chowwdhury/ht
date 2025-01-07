14c3ba1604962b7dcf3afc5741ad239f6eb87ea1
sohag-chowwdhury <sohag02.c@gmail.com>
2025-01-07 19:34:43 +0600

    add

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import { type Metadata } from 'next'
 
 import { RootLayout } from '@/components/RootLayout'
+import { GoogleAnalytics } from '@next/third-parties/google'
 
 import '@/styles/tailwind.css'
 
@@ -16,29 +17,10 @@ export default function Layout({ children }: { children: React.ReactNode }) {
     <html lang="en" className="h-full bg-neutral-950 text-base antialiased">
         <head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-WZV5J89X');
-            `,
-          }}
-        />
       </head>
       <body className="flex min-h-full flex-col">
-      <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-WZV5J89X"
-            height="0"
-            width="0"
-            style={{ display: 'none', visibility: 'hidden' }}
-          ></iframe>
-        </noscript>
         <RootLayout>{children}</RootLayout>
+        <GoogleAnalytics gaId="G-ZN49HC74QE" />
       </body>
     </html>
   )
